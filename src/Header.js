31db54f5d5c1a544392f133ec4cdd6da9cece482
sebaTruisi <sebatruisi@gmail.com>
2021-02-17 18:26:12 -0300

    Agregando header

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component{
+    render(){
+        return(
+            <header>
+                <div className='titulo'>Juego de coincidencias</div>
+                <div>
+                    <button className='boton-reiniciar'>
+                        Reiniciar
+                    </button>
+                </div>
+                <div className='titulo'>Intentos: </div>
+            </header>
+        );
+    }
+}
